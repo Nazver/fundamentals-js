5dc4cd3ca11f50997a009849fe2332aa92c0a63a
Евгений <jeka050616@gmail.com>
2024-12-05 11:40:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function concatStrings(a, b) {
+    return a + b;
+}
 
+function isString(value) {
+    return typeof value === 'string';
+}
+
+function identifySign(num) {
+    if (num > 0) {
+        return 'Положительное число';
+    } else if (num < 0) {
+        return 'Отрицательное число';
+    } else {
+        return 'Ноль';
+    }
+}
+
+function reverseWords(text) {
+    return text.split(' ').reverse().join(' ');
+}
+
+function wordsCount(text) {
+    if (!text.trim()) return 0;
+    return text.split(/\s+/).length;
+}
+
+export { concatStrings, isString, identifySign, reverseWords, wordsCount };
